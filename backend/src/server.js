ff09a37c8f5a9cc7a9dbc7fc542a1da466e07d13
vishalgarige <vishalgarige366@gmail.com>
2026-04-29 14:58:35 +0530

    fixed port for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,9 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-
 app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected ✅'))
@@ -19,12 +17,13 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/tasks', taskRoutes);
 
-
 app.get('/', (req, res) => {
   res.send('Server running 🚀');
 });
 
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
